3cba53256c56639a420e4a195b387bcff15e1b49
ELDADBRHANO <ebrhano@gmail.com>
2022-12-04 14:31:57 +0200

    nav fixed

--- client/src/components/features/haeder/header.jsx
+++ client/src/components/features/haeder/header.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
-import './header.css'
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import "./header.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 function Header() {
   return (
-<div   className='navbar-div'>
-<nav id='nav_container' className="navbar navbar-expand-lg">
-  <img id='multiLogo' src="https://logopond.com/logos/8e67d2f5a085b74d9bb658edc7c919a6.png" alt="" />
-</nav>
+    <div className="navbar-div">
+      <nav id="nav_container" className="navbar navbar-expand-lg">
+        <img
+          id="multiLogo"
+          src="./images/multimarket.png"
+          alt=""
+        />
+        <div id="dashboard">
+
+      <p className="text-center text-white">
+      Dashboard 
+      </p>
+      <div id="div_burger">
+      <img id="burger_logo" src="./images/menu.png"/>
+      </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
