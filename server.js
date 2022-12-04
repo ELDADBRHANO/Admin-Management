d4b8ea709c584b792aa5388f32ceb873684769b9
ELDADBRHANO <ebrhano@gmail.com>
2022-12-04 18:50:10 +0200

    change path to index,html

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.listen(PORT,()=>{
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/build'));
+  app.use(express.static(__dirname + '/client/build'));
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
